6624c02939a2baf28bb05aff396cd6e0d2b34553
khalees2 <salmank640@gmail.com>
2023-12-02 14:09:28 +0000

    Implemented a Custom Filter

--- src/main/java/com/practice/springsecurity/security/SpringSecBasicConfiguration.java
+++ src/main/java/com/practice/springsecurity/security/SpringSecBasicConfiguration.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -18,6 +19,7 @@ public class SpringSecBasicConfiguration {
             requests.requestMatchers("/accounts","/account/**").authenticated();
             requests.requestMatchers("/welcome","/about","/contact").permitAll();
         });
+        http.addFilterAfter(new SpringSecBasicFilterImpl(), BasicAuthenticationFilter.class);
         http.formLogin(withDefaults());
         http.httpBasic(withDefaults());
         return http.build();
