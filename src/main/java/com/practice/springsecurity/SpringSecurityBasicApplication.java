d3896444771dc29063b25e44a3085853245b1c21
khalees2 <salmank640@gmail.com>
2023-12-03 09:57:51 +0000

    Removed debug logging configuration

--- src/main/java/com/practice/springsecurity/SpringSecurityBasicApplication.java
+++ src/main/java/com/practice/springsecurity/SpringSecurityBasicApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 //Not recommended for production
-@EnableWebSecurity(debug = true)
+//@EnableWebSecurity(debug = true)
 @SpringBootApplication
 public class SpringSecurityBasicApplication {
 
